0228436f4e7153437460b8fe6698ca950db04033
Linepixer <diazmatias@linepixer.com>
2025-04-04 00:07:42 -0300

    Agregar curva SVG estilizada al layout de login

--- runserver_ssl.py
+++ runserver_ssl.py
@@ -1,24 +1,27 @@
 import os
-from django.core.management import execute_from_command_line
-from django.core.servers.basehttp import get_internal_wsgi_application
 import ssl
+from django.core.servers.basehttp import get_internal_wsgi_application
 from wsgiref.simple_server import make_server
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler as WSGIStaticFilesHandler
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
-# Configurar el contexto SSL con la librería estándar de Python
+# Crear aplicación WSGI
+application = get_internal_wsgi_application()
+
+# Activar handler de archivos estáticos si estamos en modo desarrollo
+if settings.DEBUG:
+    application = WSGIStaticFilesHandler(application)
+
+# Contexto SSL
 context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 context.load_cert_chain(certfile='api/ssl/cert.crt', keyfile='api/ssl/key.key')
 
-# Aplicación Django
-application = get_internal_wsgi_application()
-
-# Crear servidor WSGI
+# Servidor WSGI con SSL
 httpd = make_server('localhost', 8000, application)
-
-# Aplicar SSL al servidor
 httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 
-# Iniciar el servidor en HTTPS
 print("Servidor HTTPS corriendo en https://localhost:8000/")
 httpd.serve_forever()
